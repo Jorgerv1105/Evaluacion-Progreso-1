96a13f09e07d44dd96ce3ccbedd857115b8b29f2
Laboratorio <laboratorio@udla-ec.int>
2023-10-24 14:46:32 -0500

    Uso correcto de sintaxis de for y añadi otra variable

--- progreso_1.c
+++ progreso_1.c
@@ -3,9 +3,13 @@
 #include<math.h>
 int main(){
     int numero,i;
+    float sumatoria;
     printf("Ingrese un numero que sera tomado como n:");
     scanf("%d",&numero);
-    for(i=1;i<numero;i++);
-    printf("EL numero es el :%d",numero);
+    for(i=1;i<=numero;i++){
+        sumatoria=1/2;
+        printf("EL numero es el :%f",sumatoria);
+    }
+
     return 0;
 }
